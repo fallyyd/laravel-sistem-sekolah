2cec4de29bb18880d3a159635ab5751f4c0d3cd8
fallyyd <www.wilsonchristian99@gmail.com>
2026-07-30 13:06:04 +0700

    implement view

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,22 +8,42 @@ class StudentController extends Controller
 {
     public function index()
     {
-        return "Ini adalah halaman daftar siswa";
+        $title = 'Sistem Sekolah - Daftar Siswa';
+        $students = [
+            [
+                'id' => 1,
+                'nis' => '1001',
+                'name' => 'Andi',
+                'class' => 'XII TKJ 2',
+                'major' => 'TKJ'
+            ],
+            [
+                'id' => 2,
+                'nis' => '1002',
+                'name' => 'Budi',
+                'class' => 'XII TKJ 2',
+                'major' => 'TKJ'
+            ],
+        ];
+        return view('students.index', [
+            'title' => $title,
+            'students' => $students
+        ]);
     }
 
     public function show(string $id)
     {
-        return "Menampilkan detail siswa dengan ID : {$id}";
+        return view('students.show');
     }
 
     public function create()
     {
-        return "Menampilkan halaman untuk tambah data siswa";
+        return view('students.create');
     }
 
     public function edit(string $id)
     {
-        return "Menampilkan halaman edit siswa";
+        return view('students.edit');
     }
 
     public function store(Request $request)
